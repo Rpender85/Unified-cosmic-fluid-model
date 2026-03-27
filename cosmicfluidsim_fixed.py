c80882166eac2b872fd76b5000125fb736360f5f
Rpender85 <ralphpender@gmail.com>
2026-03-27 10:52:06 -0400

    Delete cosmicfluidsim_fixed.py

--- cosmicfluidsim_fixed.py
+++
@@ -1,9 +0,0 @@
-# Other code...
-# Line 25 after fixing
-    # Fixed indentation and removed stray 't'
-    print('Fixed line')
-
-# Other code...
-# Line 34 after fixing
-    # Fixed indentation with correct alignment
-    print('Fixed line again')
